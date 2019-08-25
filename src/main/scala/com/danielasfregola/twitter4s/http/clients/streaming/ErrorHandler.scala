bed2adcf1240ab475f715f367698eef9d66689df
daming-qokka <daming@qokka.ai>
2019-08-24 18:31:08 -0700

    add better error handling

--- src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/ErrorHandler.scala
+++ src/main/scala/com/danielasfregola/twitter4s/http/clients/streaming/ErrorHandler.scala
@@ -1,7 +1,11 @@
 package com.danielasfregola.twitter4s.http.clients.streaming
+import com.typesafe.scalalogging.LazyLogging
 
-object ErrorHandler {
+object ErrorHandler extends LazyLogging {
   def ignore: PartialFunction[Throwable, Unit] = {
-    case scala.util.control.NonFatal(e) => ()
+    case scala.util.control.NonFatal(e) => {
+      print("\nAn error has occurred!\n")
+      logger.warn("Default error handling", e)
+    }
   }
 }
